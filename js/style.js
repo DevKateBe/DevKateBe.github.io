55d5a88f38887d3383cc5e23debc4e452417e2ef
DevKateBe <67160686+DevKateBe@users.noreply.github.com>
2020-06-19 19:01:31 +0300

    full site

---
+++ js/style.js
@@ -0,0 +1,4 @@
+let burgerBtn = document.querySelector(".hum-nav");
+let menu = document.querySelector(".header-menu");
+
+burgerBtn.addEventListener("click", () => menu.classList.toggle("active"));
